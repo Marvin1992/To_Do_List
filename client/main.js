cb79cffcb856d49373769709146009050e78e6b1
Marvin Baumgart <marvinbaumgart18@gmail.com>
2015-02-28 14:45:23 -0600

    added routing
    deleted main,js, moved the subscription to the router, added spin
    package for visual feedback, added layout and loading template

--- client/main.js
+++
@@ -1,3 +0,0 @@
-// subscribe to the publication on the server side
-// Publication: Any data you subscribe will be mirrored on the client with the help of the MiniMongo
-Meteor.subscribe('the_to_do_list');
