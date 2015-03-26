44529a81e43619a11aa51fa3075fca439a31b1c5
Marvin Baumgart <marvinbaumgart18@gmail.com>
2015-03-26 13:48:38 -0500

    monthly and yearly timed FIXED

--- client/templates/listView/timeBased/monthlyTimed.js
+++ client/templates/listView/timeBased/monthlyTimed.js
@@ -1,19 +1,5 @@
 Template.monthlyTimed.helpers({
-	toDoMonthly: function() {
-		// get the current user
-		var user = Meteor.user();
-
-		// get the user's day
-		var currentDate = new Date();
-		var _month = currentDate.getMonth() + 1;
-		var _year = currentDate.getFullYear();
-
-		if(user){
-			// return the to-dos for this month and sort them ascending
-			return To_Dos.find({author: user.username, month: _month, year: _year}, {sort: {day: 1}});
-		}
-	},
-
+	
 	datesInCurrentMonth: function() {
 		// get the user's current month
 		var currentDate = new Date();
@@ -22,7 +8,7 @@ Template.monthlyTimed.helpers({
 
 		var days = [];
 		for (var i = 1; i <= daysInMonth(_month+1, _year); i++) {
-			// console.log(new Date(_year, _month, i));
+			//console.log(new Date(_year, _month, i));
 			days.push(new Date(_year, _month, i));
 		}
 
