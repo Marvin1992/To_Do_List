656f24b8f58446001090a93c8003fb8ac26c8a3f
Marvin Baumgart <marvinbaumgart18@gmail.com>
2015-03-26 18:23:45 -0500

    attempt to restructure weekly and daily.js with templates

--- client/templates/listView/timeBased/dailyTimed.js
+++ client/templates/listView/timeBased/dailyTimed.js
@@ -1,17 +1,45 @@
 Template.dailyTimed.helpers({
-	toDoToday: function() {
-		// get the current user
-		var user = Meteor.users.findOne();
+	hours: function() {
+		// get the user's current month
+		var currentDate = new Date();
+		var _day = currentDate.getDate();
+		var _month = currentDate.getMonth();
+		var _year = currentDate.getFullYear();
 
+		// array that holds our hours
+		var hours = [];
+		for (var i = 1; i <= 24; i++) {
+			hours.push(new Date(_year, _month, _day, i));
+		}
+
+		return hours;
+	},
+	toDoToday: function() {
 		// get the user's day
 		var currentDate = new Date();
+		var _hour = currentDate.getHours();
 		var _day = currentDate.getDate();
 		var _month = currentDate.getMonth() + 1;
 		var _year = currentDate.getFullYear();
 
-		if(user){
-			// return the to-dos for today that belong to the current user
-			return To_Dos.find({author: user.username, day: _day, month: _month, year: _year});
-		}
+
+		// return the to-dos for today that belong to the current user
+		return To_Dos.find({
+			author: Meteor.user().username, 
+			day: _day, 
+			month: _month, 
+			year: _year
+		});
 	}
+	/*
+	toDoToday: function() {
+		return To_Dos.find({
+			author: Meteor.user().username, 
+			hour: this.getHours(),
+			month: this.getMonth() + 1, 
+			year: this.getFullYear(),
+			day: this.getDate()  
+		});
+	}
+	*/
 });
