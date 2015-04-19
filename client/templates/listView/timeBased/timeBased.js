771eba8f225b7dc48d821fe4cfec16e18109eb83
Marvin Baumgart <marvinbaumgart18@gmail.com>
2015-04-18 23:38:08 -0500

    .....

---
+++ client/templates/listView/timeBased/timeBased.js
@@ -0,0 +1,10 @@
+Template.timeBased.helpers({
+	taskCount: function(){
+		// if the counter is at 0
+		if($('.task-reminder').text() == 0){
+			return 'hasAccomplished';
+		} else {
+			return '';
+		}
+	}
+});
